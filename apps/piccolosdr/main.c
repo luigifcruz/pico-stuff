249baf2f6829d4badc60f4a0487990ffd3f699af
Luigi Cruz <luigifcruz@gmail.com>
2021-04-29 00:46:48 -0300

    it aint pretty but works better than main

--- apps/piccolosdr/main.c
+++ apps/piccolosdr/main.c
@@ -11,40 +11,29 @@ bool streaming = false;
 struct tcp_pcb* client;
 struct repeating_timer timer;
 
-//#define DEBUG
 #define CAPTURE_CHANNEL 0
 #define CAPTURE_DEPTH 500
 
+uint head = 0;
+bool dataval = false;
 uint dma_chan_a, dma_chan_b;
 uint8_t capture_buf_a[CAPTURE_DEPTH];
 uint8_t capture_buf_b[CAPTURE_DEPTH];
 
-void dma_handler(uint8_t* buffer, int id) {
-#ifdef DEBUG
-    char str[64];
-    int len = sprintf(str, "DMA IRQ %d [%d %d ... %d]\n", id, buffer[0], buffer[1], buffer[CAPTURE_DEPTH-1]);
-    tcp_write(client, str, len, 0);
-    tcp_output(client);
-#else
-    tcp_write(client, buffer, CAPTURE_DEPTH, 0x01);
-    tcp_output(client);
-#endif
-}
-
-void dma_handler_a() {
-    dma_handler((uint8_t*)&capture_buf_a, 0);
+static void dma_handler_a() {
+    dataval = true;
+    head = 0;
     dma_hw->ints0 = 1u << dma_chan_a;
-    dma_channel_set_write_addr(dma_chan_a, &capture_buf_a, false);
 }
 
-void dma_handler_b() {
-    dma_handler((uint8_t*)&capture_buf_b, 1);
+static void dma_handler_b() {
+    dataval = true;
+    head = 1;
     dma_hw->ints1 = 1u << dma_chan_b;
-    dma_channel_set_write_addr(dma_chan_b, &capture_buf_b, false);
 }
 
 static void init_adc_dma_chain() {
-adc_gpio_init(26 + CAPTURE_CHANNEL);
+    adc_gpio_init(26 + CAPTURE_CHANNEL);
     adc_init();
     adc_select_input(CAPTURE_CHANNEL);
     adc_fifo_setup(
@@ -95,10 +84,12 @@ adc_gpio_init(26 + CAPTURE_CHANNEL);
 
     dma_channel_set_irq0_enabled(dma_chan_a, true);
     irq_set_exclusive_handler(DMA_IRQ_0, dma_handler_a);
+    irq_set_priority(DMA_IRQ_0, 0xFF);
     irq_set_enabled(DMA_IRQ_0, true);
 
     dma_channel_set_irq1_enabled(dma_chan_b, true);
     irq_set_exclusive_handler(DMA_IRQ_1, dma_handler_b);
+    irq_set_priority(DMA_IRQ_1, 0xFF);
     irq_set_enabled(DMA_IRQ_1, true);
 
     adc_run(false);
@@ -134,7 +125,7 @@ static void srv_err(void *arg, err_t err) {
 }
 
 static err_t srv_receive(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err) {
-    if (err != ERR_OK && p != NULL) {
+    if (err != ERR_OK) {
         goto exception;
     }
 
@@ -147,7 +138,10 @@ static err_t srv_receive(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t e
     }
 
 exception:
-    pbuf_free(p);
+    if (p != NULL) {
+        pbuf_free(p);
+    }
+
     return err;
 }
 
@@ -155,8 +149,8 @@ static err_t srv_accept(void * arg, struct tcp_pcb * pcb, err_t err) {
     if (err != ERR_OK) {
         return err;
     }
-        
-    tcp_setprio(pcb, TCP_PRIO_MAX);
+
+    tcp_setprio(pcb, TCP_PRIO_MIN);
     tcp_recv(pcb, srv_receive);
     tcp_err(pcb, srv_err);
     tcp_poll(pcb, NULL, 4);
@@ -167,7 +161,7 @@ static err_t srv_accept(void * arg, struct tcp_pcb * pcb, err_t err) {
     return err;
 }
 
-bool led_timer(struct repeating_timer *t) {
+static bool led_timer(struct repeating_timer *t) {
     int status = 1;
     if (streaming) {
         status = !gpio_get(PICO_DEFAULT_LED_PIN);
@@ -203,6 +197,22 @@ int main(void) {
     // Listen to events.
     while (1) {
         network_step();
+
+        if (dataval) {
+            dataval = false;
+
+            if (head == 0) {
+                tcp_write(client, &capture_buf_a, CAPTURE_DEPTH, TCP_WRITE_FLAG_COPY);
+                dma_channel_set_write_addr(dma_chan_a, &capture_buf_a, false);
+            }
+
+            if (head == 1) {
+                tcp_write(client, &capture_buf_b, CAPTURE_DEPTH, TCP_WRITE_FLAG_COPY);
+                dma_channel_set_write_addr(dma_chan_b, &capture_buf_b, false);
+            }
+
+            tcp_output(client);
+        }
     }
 
     return 0;
---
+++ apps/piccolosdr/piccolosdr.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: PiccoloSDR
+# Author: Luigi Cruz
+# Copyright: MIT
+# GNU Radio version: 3.8.2.0
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print("Warning: failed to XInitThreads()")
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
+from gnuradio import blocks
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import grnet
+
+from gnuradio import qtgui
+
+class piccolosdr(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "PiccoloSDR")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("PiccoloSDR")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "piccolosdr")
+
+        try:
+            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+                self.restoreGeometry(self.settings.value("geometry").toByteArray())
+            else:
+                self.restoreGeometry(self.settings.value("geometry"))
+        except:
+            pass
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 500000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_f(
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1 #number of inputs
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+
+        self.qtgui_waterfall_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            1024, #size
+            samp_rate, #samp_rate
+            "", #name
+            1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
+            1024, #size
+            firdes.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+
+
+        self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.grnet_tcp_source_0 = grnet.tcp_source.tcp_source(
+        	itemsize=gr.sizeof_char*500,
+        	addr='192.168.7.1',
+        	port=7777,
+        	server=False,
+        )
+        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_char*1, 500)
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(3.3 / 255.0)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_vector_to_stream_0, 0), (self.blocks_uchar_to_float_0, 0))
+        self.connect((self.grnet_tcp_source_0, 0), (self.blocks_vector_to_stream_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "piccolosdr")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
+
+
+
+
+
+def main(top_block_cls=piccolosdr, options=None):
+
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
