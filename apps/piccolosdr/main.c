21d4f22c8b6ea134d6e9c9c4f7a5bf1bdfa57ff7
Luigi Cruz <luigifcruz@gmail.com>
2021-05-02 21:36:21 -0600

    Replace TCP with UDP. (#1)
    * replace tcp with udp
    * remove python grc

--- apps/piccolosdr/main.c
+++ apps/piccolosdr/main.c
@@ -7,40 +7,36 @@
 #include "hardware/irq.h"
 #include "usb_network.h"
 
-bool streaming = false;
-struct tcp_pcb* client;
+uint data_ovf;
+uint data_dma;
+bool data_val;
+bool streaming;
 struct repeating_timer timer;
 
-//#define DEBUG
 #define CAPTURE_CHANNEL 0
-#define CAPTURE_DEPTH 500
+#define CAPTURE_DEPTH 1472
 
 uint dma_chan_a, dma_chan_b;
-uint8_t capture_buf_a[CAPTURE_DEPTH];
-uint8_t capture_buf_b[CAPTURE_DEPTH];
+struct pbuf *pbuf_a, *pbuf_b;
 
-void dma_handler(uint8_t* buffer, int id) {
-#ifdef DEBUG
-    char str[64];
-    int len = sprintf(str, "DMA IRQ %d [%d %d ... %d]\n", id, buffer[0], buffer[1], buffer[CAPTURE_DEPTH-1]);
-    tcp_write(client, str, len, 0);
-    tcp_output(client);
-#else
-    tcp_write(client, buffer, CAPTURE_DEPTH, 0x01);
-    tcp_output(client);
-#endif
+static void dma_handler(uint id) {
+    if (data_val) {
+        data_ovf += 1;
+    }
+    data_dma = id;
+    data_val = true;
 }
 
-void dma_handler_a() {
-    dma_handler((uint8_t*)&capture_buf_a, 0);
+static void dma_handler_a() {
+    dma_handler(0);
+    dma_channel_set_write_addr(dma_chan_a, pbuf_a->payload, false);
     dma_hw->ints0 = 1u << dma_chan_a;
-    dma_channel_set_write_addr(dma_chan_a, &capture_buf_a, false);
 }
 
-void dma_handler_b() {
-    dma_handler((uint8_t*)&capture_buf_b, 1);
+static void dma_handler_b() {
+    dma_handler(1);
+    dma_channel_set_write_addr(dma_chan_b, pbuf_b->payload, false);
     dma_hw->ints1 = 1u << dma_chan_b;
-    dma_channel_set_write_addr(dma_chan_b, &capture_buf_b, false);
 }
 
 static void init_adc_dma_chain() {
@@ -80,14 +76,14 @@ static void init_adc_dma_chain() {
     channel_config_set_chain_to(&dma_cfg_b, dma_chan_a);
 
     dma_channel_configure(dma_chan_a, &dma_cfg_a,
-        capture_buf_a,  // dst
+        pbuf_a->payload,// dst
         &adc_hw->fifo,  // src
         CAPTURE_DEPTH,  // transfer count
         true            // start now
     );
 
     dma_channel_configure(dma_chan_b, &dma_cfg_b,
-        capture_buf_b,  // dst
+        pbuf_b->payload, // dst
         &adc_hw->fifo,  // src
         CAPTURE_DEPTH,  // transfer count
         false           // start now
@@ -106,70 +102,38 @@ static void init_adc_dma_chain() {
     adc_run(false);
 }
 
-static void start_stream() {
-    dma_channel_set_write_addr(dma_chan_a, &capture_buf_a, true);
-    dma_channel_set_write_addr(dma_chan_b, &capture_buf_b, false);
-    adc_run(true);
+static void start_stream(struct tcp_pcb *pcb) {
+    if (streaming) {
+        return;
+    }
+
+    data_ovf = 0;
+    data_dma = 0;
+    data_val = false;
     streaming = true;
+
+    dma_channel_set_write_addr(dma_chan_a, pbuf_a->payload, true);
+    dma_channel_set_write_addr(dma_chan_b, pbuf_b->payload, false);
+    adc_run(true);
 }
 
-static void stop_stream() {
+static void stop_stream(struct tcp_pcb *pcb) {
+    if (!streaming) {
+        return;
+    }
+
     adc_run(false);
     adc_fifo_drain();
-    dma_channel_set_write_addr(dma_chan_a, &capture_buf_a, false);
-    dma_channel_set_write_addr(dma_chan_b, &capture_buf_b, false);
+    dma_channel_set_write_addr(dma_chan_a, pbuf_a->payload, false);
+    dma_channel_set_write_addr(dma_chan_b, pbuf_b->payload, false);
+
+    data_ovf = 0;
+    data_dma = 0;
+    data_val = false;
     streaming = false;
 }
 
-static void srv_close(struct tcp_pcb *pcb){
-    stop_stream();
-
-    tcp_arg(pcb, NULL);
-    tcp_sent(pcb, NULL);
-    tcp_recv(pcb, NULL);
-    tcp_close(pcb);
-}
-
-static void srv_err(void *arg, err_t err) {
-    // Probably an indication that the client connection went kaput! Stopping stream...
-    srv_close(client);
-}
-
-static err_t srv_receive(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err) {
-    if (err != ERR_OK && p != NULL) {
-        goto exception;
-    }
-
-    tcp_recved(pcb, p->tot_len);
-    tcp_sent(pcb, NULL);
-
-    // The connection is closed if the client sends "X".
-    if (((char*)p->payload)[0] == 'X') {
-        srv_close(pcb);
-    }
-
-exception:
-    pbuf_free(p);
-    return err;
-}
-
-static err_t srv_accept(void * arg, struct tcp_pcb * pcb, err_t err) {
-    if (err != ERR_OK) {
-        return err;
-    }
-
-    tcp_setprio(pcb, TCP_PRIO_MIN);
-    tcp_recv(pcb, srv_receive);
-    tcp_err(pcb, srv_err);
-    tcp_poll(pcb, NULL, 4);
-
-    client = pcb;
-    start_stream();
-
-    return err;
-}
-
-bool led_timer(struct repeating_timer *t) {
+static bool led_timer(struct repeating_timer *t) {
     int status = 1;
     if (streaming) {
         status = !gpio_get(PICO_DEFAULT_LED_PIN);
@@ -183,27 +147,45 @@ int main(void) {
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    // Init ADC DMA chain.
-    init_adc_dma_chain();
-
     // Init network stack.
     network_init();
 
-    // Start TCP server.
-    struct tcp_pcb* pcb = tcp_new();
-    pcb->so_options |= SOF_KEEPALIVE;
-    pcb->keep_intvl = 75000000;
-    tcp_bind(pcb, IP_ADDR_ANY, 7777);
+    // Allocate zero-copy memory for DMA and UDP.
+    pbuf_a = pbuf_alloc(PBUF_RAW, CAPTURE_DEPTH, PBUF_RAM);
+    pbuf_b = pbuf_alloc(PBUF_RAW, CAPTURE_DEPTH, PBUF_RAM);
 
-    // Start listening for connections.
-    struct tcp_pcb* listen = tcp_listen(pcb);
-    tcp_accept(listen, srv_accept);
+    if (pbuf_a == NULL && pbuf_b == NULL) {
+        return 1;
+    }
+
+    // Init ADC DMA chain.
+    init_adc_dma_chain();
+
+    // Start data UDP port.
+    ip_addr_t client;
+    struct udp_pcb* dpcb = udp_new();
+    IP4_ADDR(&client, 192, 168, 7, 2);
+    udp_connect(dpcb, &client, 7778);
 
     // Start LED indicator.
     add_repeating_timer_ms(250, led_timer, NULL, &timer);
 
+    // Start stream right away.
+    start_stream(NULL);
+
     // Listen to events.
     while (1) {
+        if (data_val && streaming) {
+            if (data_dma == 0) {
+                udp_send(dpcb, pbuf_a);
+            }
+            if (data_dma == 1) {
+                udp_send(dpcb, pbuf_b);
+            }
+
+            data_val = false;
+        }
+
         network_step();
     }
 
