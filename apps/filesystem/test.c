2ddd9e14704e35a8914b7d68ac1dba79c7390a8a
Luigi F. Cruz <luigifcruz@gmail.com>
2023-01-28 19:11:59 -0800

    Add littlefs logic.

--- apps/filesystem/test.c
+++ apps/filesystem/test.c
@@ -1,42 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "hardware/flash.h"
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 
-#include <lfs.h>
 #include <lfs_rp2040.h>
 
 // variables used by the filesystem
 lfs_t lfs;
 lfs_file_t file;
-
-// configuration of the filesystem is provided by this struct
-const struct lfs_config cfg = {
-    // block device operations
-    .read  = lfs_rp2040_read,
-    .prog  = lfs_rp2040_prog,
-    .erase = lfs_rp2040_erase,
-    .sync  = lfs_rp2040_sync,
-
-    // block device configuration
-    .read_size = FLASH_PAGE_SIZE,
-    .prog_size = FLASH_PAGE_SIZE,
-    .block_size = FLASH_SECTOR_SIZE,
-    .block_count = 128,
-    .cache_size = FLASH_PAGE_SIZE,
-
-    .lookahead_size = 16,
-    .block_cycles = 500,
-};
-
-uint storage_get_flash_capacity() {
-    uint8_t rxbuf[4];
-    uint8_t txbuf[] = {0x9f};
-    flash_do_cmd(txbuf, rxbuf, 4);
-    return 1 << rxbuf[3];
-}
 
 // entry point
 int main(void) {
@@ -46,16 +18,14 @@ int main(void) {
         sleep_ms(10);
     }
 
-    printf("%d\n", storage_get_flash_capacity());
-
     printf("Hello from Pi Pico!\n");
+
+    const struct lfs_config cfg = lfs_rp2040_init();
 
     // mount the filesystem
     int err = lfs_mount(&lfs, &cfg);
 
     printf("%d\n\n", err);
-
-    return 0;
 
     // reformat if we can't mount the filesystem
     // this should only happen on the first boot
@@ -82,4 +52,8 @@ int main(void) {
 
     // print the boot count
     printf("boot_count: %d\n", boot_count);
+
+    while(true) {
+        sleep_ms(1000);
+    }
 }
--- lib/littlefs/lfs_rp2040.h
+++ lib/littlefs/lfs_rp2040.h
@@ -1,28 +1,27 @@
 #ifndef LFS_RP2040_H
 #define LFS_RP2040_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <lfs.h>
 #include <lfs_util.h>
 
-// Read a region in a block. Negative error codes are propagated
-// to the user.
+#include "hardware/flash.h"
+#include "hardware/sync.h"
+#include "pico/stdio.h"
+#include "pico/stdlib.h"
+
+const struct lfs_config lfs_rp2040_init();
+
 int lfs_rp2040_read(const struct lfs_config *c, lfs_block_t block,
                     lfs_off_t off, void *buffer, lfs_size_t size);
 
-// Program a region in a block. The block must have previously
-// been erased. Negative error codes are propagated to the user.
-// May return LFS_ERR_CORRUPT if the block should be considered bad.
 int lfs_rp2040_prog(const struct lfs_config *c, lfs_block_t block,
                     lfs_off_t off, const void *buffer, lfs_size_t size);
 
-// Erase a block. A block must be erased before being programmed.
-// The state of an erased block is undefined. Negative error codes
-// are propagated to the user.
-// May return LFS_ERR_CORRUPT if the block should be considered bad.
 int lfs_rp2040_erase(const struct lfs_config *c, lfs_block_t block);
 
-// Sync the state of the underlying block device. Negative error codes
-// are propagated to the user.
 int lfs_rp2040_sync(const struct lfs_config *c);
 
 #endif
